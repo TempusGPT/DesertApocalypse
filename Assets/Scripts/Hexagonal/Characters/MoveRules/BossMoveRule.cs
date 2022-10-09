d57657ceccada7ea339d4bfa25eba6b709431f58
tempus157 <tempus157@gmail.com>
2022-10-09 22:09:53 +0900

    Add boss move rule

---
+++ Assets/Scripts/Hexagonal/Characters/MoveRules/BossMoveRule.cs
@@ -0,0 +1,6 @@
+using System.Linq;
+using UnityEngine;
+
+public class BossMoveRule : MoveRule {
+    public override void Move(EnemyController controller, Tile playerTile) { }
+}
--- Assets/Scripts/Hexagonal/Characters/MoveRules/ZakoMoveRule.cs
+++ Assets/Scripts/Hexagonal/Characters/MoveRules/ZakoMoveRule.cs
@@ -1,7 +1,7 @@
 using System.Linq;
 using UnityEngine;
 
-public class HumanWarriorMoveRule : MoveRule {
+public class ZakoMoveRule : MoveRule {
     public override void Move(EnemyController controller, Tile playerTile) {
         var nearTiles = controller.TileTransform.Tile.NearTiles.Values;
         var targetTile = nearTiles.ElementAt(Random.Range(0, nearTiles.Count));
