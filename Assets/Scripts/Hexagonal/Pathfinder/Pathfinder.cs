79e6c901a5bdb10bf38ebd4961d8232639170d7a
tempus157 <tempus157@gmail.com>
2022-11-10 04:51:12 +0900

    패스파인더 최적화

--- Assets/Scripts/Hexagonal/Pathfinder/Pathfinder.cs
+++ Assets/Scripts/Hexagonal/Pathfinder/Pathfinder.cs
@@ -2,50 +2,54 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Pathfinder {
+    private readonly HashSet<Tile> openSet = new();
+    private readonly Dictionary<Tile, Tile> cameFrom = new();
+    private readonly Dictionary<Tile, float> gScore = new();
+    private readonly Dictionary<Tile, float> fScore = new();
+
     public IEnumerable<Tile> Find(Tile start, Tile goal) {
-        var openSet = new HashSet<Tile> {
-            start
-        };
-
-        var cameFrom = new Dictionary<Tile, Tile>();
-
-        var gScore = new Dictionary<Tile, float> {
-            [start] = 0
-        };
-
-        var fScore = new Dictionary<Tile, float> {
-            [start] = Tile.Distance(start, goal)
-        };
+        openSet.Clear();
+        openSet.Add(start);
+        cameFrom.Clear();
+        gScore.Clear();
+        gScore[start] = 0;
+        fScore.Clear();
+        fScore[start] = Tile.Distance(start, goal);
 
         while (openSet.Count > 0) {
             var current = openSet.Aggregate(
-                (a, b) => GetScore(fScore, a) < GetScore(fScore, b) ? a : b
+                (a, b) => GetFScore(a) < GetFScore(b) ? a : b
             );
             if (current == goal) {
-                return ReconstructPath(cameFrom, current);
+                return ReconstructPath(cameFrom, current).Reverse();
             }
 
             openSet.Remove(current);
-            foreach (var neighbor in current.NearTiles.Values) {
-                var tentativeG = GetScore(gScore, current)
-                    + Tile.Distance(current, neighbor);
-                if (tentativeG < GetScore(gScore, neighbor)) {
-                    cameFrom[neighbor] = current;
-                    gScore[neighbor] = tentativeG;
-                    fScore[neighbor] = tentativeG + Tile.Distance(neighbor, goal);
+            foreach (var near in current.NearTiles.Values) {
+                var tentativeG = GetGScore(current) + Tile.Distance(current, near);
+                if (tentativeG < GetGScore(near)) {
+                    cameFrom[near] = current;
+                    gScore[near] = tentativeG;
+                    fScore[near] = tentativeG + Tile.Distance(near, goal);
 
-                    if (!openSet.Contains(neighbor)) {
-                        openSet.Add(neighbor);
+                    if (!openSet.Contains(near)) {
+                        openSet.Add(near);
                     }
                 }
             }
         }
-        
+
         return Nothing();
     }
 
-    private static float GetScore(IReadOnlyDictionary<Tile, float> score, Tile tile) {
-        return score.TryGetValue(tile, out var result)
+    private float GetGScore(Tile tile) {
+        return gScore.TryGetValue(tile, out var result)
+            ? result
+            : float.PositiveInfinity;
+    }
+
+    private float GetFScore(Tile tile) {
+        return fScore.TryGetValue(tile, out var result)
             ? result
             : float.PositiveInfinity;
     }
@@ -54,14 +58,10 @@ public class Pathfinder {
         Dictionary<Tile, Tile> cameFrom,
         Tile current
     ) {
-        var totalPath = new List<Tile> {
-            current
-        };
         while (cameFrom.Keys.Contains(current)) {
+            yield return current;
             current = cameFrom[current];
-            totalPath.Insert(0, current);
         }
-        return totalPath.Skip(1);
     }
 
     private static IEnumerable<Tile> Nothing() {
