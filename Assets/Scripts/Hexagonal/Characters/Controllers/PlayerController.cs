c6294269d0eb1d9115dda40434153bc266fb8ef0
tempus157 <tempus157@gmail.com>
2022-11-11 00:24:11 +0900

    StageScene으로 리네임

--- Assets/Scripts/Hexagonal/Characters/Controllers/PlayerController.cs
+++ Assets/Scripts/Hexagonal/Characters/Controllers/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 [RequireComponent(typeof(TileTransform))]
@@ -20,7 +21,7 @@ public class PlayerController : MonoBehaviour {
         if (!tile.IsWalkable) {
             return;
         }
-        TileTransform.MoveTo(tile);
+        TileTransform.MoveTo(tile).Forget();
     }
 
     private static void NotifyMove(Tile tile) {
--- Assets/Scripts/Hexagonal/Characters/MoveRules/ZakoMoveRule.cs
+++ Assets/Scripts/Hexagonal/Characters/MoveRules/ZakoMoveRule.cs
@@ -1,10 +1,11 @@
 using System.Linq;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class ZakoMoveRule : MoveRule {
     public override void Move(TileTransform tileTransform, Tile targetTile) {
         var nearTiles = tileTransform.Tile.NearTiles.Values;
         var destinationTile = nearTiles.ElementAt(Random.Range(0, nearTiles.Count));
-        tileTransform.MoveTo(destinationTile);
+        tileTransform.MoveTo(destinationTile).Forget();
     }
 }
